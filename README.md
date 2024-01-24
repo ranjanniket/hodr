c6dbabf51052c7abdfaa0cc9745609f4d03ac4dc
Pratik Gajjar <pratik@fampay.in>
2024-01-24 20:44:46 +0530

    Add promhttp metrics

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hodor
 A golang http service
+
+# setup
+
+```bash
+go mod download # download deps
+go run main.go # run app
+```
+
+- App uses OS environment for config
+- http://127.0.0.1:8000/metrics - returns app metrics
+- http://127.0.0.1:8000/* - returns hodor...hodor..hodor.. 
--- main.go
+++ main.go
@@ -5,15 +5,42 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// Define Prometheus counters for request metrics
+var (
+	requestMethodCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "path"},
+	)
+	responseStatusCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_responses_total",
+			Help: "Total number of HTTP responses",
+		},
+		[]string{"status_code", "method", "path"},
+	)
 )
 
 var log *slog.Logger
 
 func init() {
 	log = slog.New(slog.NewTextHandler(os.Stdout, nil))
+	// Register Prometheus metrics
+	prometheus.MustRegister(requestMethodCounter)
+	prometheus.MustRegister(responseStatusCounter)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	// Increment request method and path counter
+	requestMethodCounter.WithLabelValues(r.Method, r.URL.Path).Inc()
+
 	// Set the response header
 	w.Header().Set("Content-Type", "text/plain")
 
@@ -27,11 +54,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		// Respond with a 405 Method Not Allowed for other request methods
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 	}
+	responseStatusCounter.WithLabelValues(fmt.Sprint(http.StatusOK), r.Method, r.URL.Path).Inc()
 }
 
 func main() {
 	// Register the handler function for all routes
 	http.HandleFunc("/", handler)
+	// Register Prometheus endpoint
+	http.Handle("/metrics", promhttp.Handler())
 
 	// Start the server on port 8080
 	log.Info("Server is running on http://localhost:8080")
